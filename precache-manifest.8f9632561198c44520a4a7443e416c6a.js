e409961dc5dcdb11feca002bd7d898325d266ef9
danielmicaletti <danielmicaletti@gmail.com>
2019-01-08 13:58:04 -0700

    Updates

---
+++ precache-manifest.8f9632561198c44520a4a7443e416c6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "76785b4fc8cca9cf570a",
+    "url": "/react_pwa/static/css/main.bf91b0dd.chunk.css"
+  },
+  {
+    "revision": "76785b4fc8cca9cf570a",
+    "url": "/react_pwa/static/js/main.76785b4f.chunk.js"
+  },
+  {
+    "revision": "46ee9c4195611a72763b",
+    "url": "/react_pwa/static/css/1.dceafb4c.chunk.css"
+  },
+  {
+    "revision": "46ee9c4195611a72763b",
+    "url": "/react_pwa/static/js/1.46ee9c41.chunk.js"
+  },
+  {
+    "revision": "f642e8a84b9c345215c6",
+    "url": "/react_pwa/static/js/runtime~main.f642e8a8.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react_pwa/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d1e535fa61361cc8afc32e9c205b98d9",
+    "url": "/react_pwa/index.html"
+  }
+];
